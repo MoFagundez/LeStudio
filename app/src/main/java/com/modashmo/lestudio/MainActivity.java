30857d042d2a347fcd45d2dd9eef59e2b76c3633
Mauricio F <fagundesm86@gmail.com>
2017-04-22 14:10:51 -0500

    Add dimen.xml & change styles.xml to improve layout code

--- app/src/main/java/com/modashmo/lestudio/MainActivity.java
+++ app/src/main/java/com/modashmo/lestudio/MainActivity.java
@@ -2,6 +2,8 @@ package com.modashmo.lestudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
